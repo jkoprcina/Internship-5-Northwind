6390e9b59463d909e734ca53393761a33b966f69
jkoprcina <koprcina.josip@gmail.com>
2019-01-15 14:27:36 +0100

    Second task done, renaming first

---
+++ Internship-5-NorthWind.TastTwo.sql
@@ -0,0 +1,27 @@
+/*First task*/
+SELECT CompanyName FROM Suppliers 
+SELECT ContactName FROM Suppliers 
+SELECT ContactTitle FROM Suppliers 
+SELECT Address FROM Suppliers 
+SELECT City FROM Suppliers 
+SELECT Phone FROM Suppliers 
+SELECT TOP 50 * FROM Customers
+
+/*Second task*/
+SELECT COUNT(CustomerID) FROM Customers
+WHERE FAX IS NOT NULL AND REGION IS NOT NULL;
+/*Third task*/
+SELECT TOP 2 FirstName, LastName, Title FROM Employees 
+ORDER BY BirthDate DESC
+/*Fourth task*/
+SELECT * FROM Customers
+WHERE Address = '[0-9]%' AND CompanyName = '__a%'
+ORDER BY Country
+/*Fifth task*/
+SELECT TOP 1 OrderId Quantity FROM [Order Details]
+ORDER BY Quantity DESC  
+/*Sixth task*/
+SELECT * FROM CUSTOMERS 
+WHERE NOT EXISTS(
+SELECT OrderID FROM ORDERS
+WHERE Customers.CustomerID=Orders.CustomerId)
